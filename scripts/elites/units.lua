ad36cd1fc8182ace7ee7e69037160180d108dc11
Crestez Leonard <n0body@noreply.example.com>
2004-02-14 22:38:32 +0000

    Attack from transporter

--- scripts/elites/units.lua
+++ scripts/elites/units.lua
@@ -26,7 +26,7 @@
 --      along with this program; if not, write to the Free Software
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
---	$Id: units.lua,v 1.7 2004/02/12 22:38:33 n0body Exp $
+--	$Id: units.lua,v 1.8 2004/02/14 22:38:32 n0body Exp $
 
 --=============================================================================
 --	Define unit-types.
@@ -63,8 +63,8 @@ DefineUnitType("unit-apcs", {
 	ComputerReactionRange = 5, PersonReactionRange = 5,
 	Armor = 6, BasicDamage = 5, PiercingDamage = 5, Missile = "missile-none",
 	MaxAttackRange = 4, Priority = 60, Points = 50,
-	Transporter = true, MaxOnBoard = 4,	Type = "land",
-	RepairHp = 2, RepairCosts = {titanium, 2},
+	Transporter = true, AttackFromTransporter = true, MaxOnBoard = 4,
+	Type = "land", RepairHp = 2, RepairCosts = {titanium, 2},
 	ExplodeWhenKilled = "missile-160x128-explosion",
 	Demand = 0, CanAttack = true, CanTargetLand = true,
 	SelectableByRectangle = true,
