829757af74c3fb5f19364674620a3941e4be448c
Francois Beerten <feb@boswars.org>
2004-07-05 20:50:23 +0000

    Updated units to use CanTransport.
    APC can only transport organic units. Fixes [bugs #8880] APC smolder unit loading. https://savannah.nongnu.org/bugs/?func=detailitem&item_id=8880

--- scripts/elites/units.lua
+++ scripts/elites/units.lua
@@ -26,7 +26,7 @@
 --      along with this program; if not, write to the Free Software
 --      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
---	$Id: units.lua,v 1.15 2004/03/09 20:11:16 feb Exp $
+--	$Id: units.lua,v 1.16 2004/07/05 20:50:23 feb Exp $
 
 --=============================================================================
 --	Define unit-types.
@@ -42,7 +42,6 @@ DefineUnitType("unit-plate1", {
 	Construction = "construction-plate",
 	Speed = 0, HitPoints = 100, DrawLevel = 1, TileSize = {1, 1}, BoxSize = {36, 36},
 	SightRange = 0, Armor = 20, BasicDamage = 0, PiercingDamage = 0, Missile = "missile-none",
-	Transporter = true, AttackFromTransporter = true, MaxOnBoard = 2,
 	Priority = 20, AnnoyComputerFactor = 45, VisibleUnderFog = true,
 	Points = 15, Type = "land", Building = true, BuilderOutside = true,
 	Sounds = {
@@ -63,7 +62,7 @@ DefineUnitType("unit-apcs", {
 	ComputerReactionRange = 5, PersonReactionRange = 5,
 	Armor = 10, BasicDamage = 5, PiercingDamage = 5, Missile = "missile-none",
 	MaxAttackRange = 4, Priority = 60, Points = 50,
-	Transporter = true, AttackFromTransporter = true, MaxOnBoard = 6,
+	CanTransport = {"organic", "only"}, AttackFromTransporter = true, MaxOnBoard = 6,
 	Type = "land", RepairHp = 2, RepairCosts = {titanium, 2},
 	RightMouseAction = "attack",
 	ExplodeWhenKilled = "missile-160x128-explosion",
