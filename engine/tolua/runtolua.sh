46b6b6b5e43577ff375d4eb3889f0a3a21beb49e
andy5995 <arch_stanton5995@protonmail.com>
2023-07-10 10:48:15 -0500

    Modify runtolua script to use toluapp if tolua++ isn't present
    On Manjaro, with the tolua++ package installed, only toluapp is
    available

--- engine/tolua/runtolua.sh
+++ engine/tolua/runtolua.sh
@@ -1,1 +1,11 @@
-tolua++ -L stratagus.lua -o tolua.cpp stratagus.pkg
+BIN=$(command -v tolua++)
+
+if [ $? != 0 ]; then
+  BIN=$(command -v toluapp)
+  if [ $? != 0 ]; then
+    echo "tolua++ is not installed"
+    exit 1
+  fi
+fi
+
+$BIN -L stratagus.lua -o tolua.cpp stratagus.pkg
